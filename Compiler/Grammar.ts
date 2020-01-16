fb65e19d76bf04f6b8836b272d05d2fcf03f6d44
Alex <kjakarproductions@gmail.com>
2020-01-16 11:58:57 -0500

    Made the grammar class

---
+++ Compiler/Grammar.ts
@@ -0,0 +1,49 @@
+export class Grammar {
+
+    File: string;
+    leftHandSides: Set<string> = new Set();
+    rightHandSides: Set<string> = new Set();
+
+    constructor(File: string) {
+        this.File = File;
+
+        let splits: string[] = this.File.split('\n');
+        
+        
+        splits.forEach((value) =>
+        {
+            if (value.length != 0)
+            {
+                console.log(" ====> : " + value);
+                let sides: string[] = value.split("->");
+                
+
+                //make sure we have the left and right hand sides
+                if (sides.length != 2) {
+                    console.log("1");
+                    console.log("sides count ='s : " + sides.length);
+                    sides.forEach((value) => { console.log(value); });
+                    throw new Error("Invalid grammar structure : " + sides[0]);
+                }
+
+                //remove the whitespace around the strings
+                sides[0] = sides[0].trim();
+                sides[1] = sides[1].trim();
+
+                //make sure we have only seen the left hand side once
+                if (this.leftHandSides.has(sides[0])) {
+                    console.log("2");
+                    throw new Error("This left hand side has been used before : " + sides[0]);
+
+                }
+                else { //make sure the regex is vaild
+                    try { let rgx = new RegExp(sides[1]); console.log( "===" + sides[1]); }
+                    catch (Exception) { console.log("3"); throw new Error("this is not a valid regex : " + sides[1]); }
+                    this.leftHandSides.add(sides[0]);
+                    this.rightHandSides.add(sides[1]);
+                }
+            }
+
+        });
+    }
+}
--- Compiler/app.js
+++ Compiler/app.js
@@ -1,2 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fs = require("fs");
+const Grammar_1 = require("./Grammar");
+function main() {
+    let data = fs.readFileSync("tests.txt", "utf8");
+    let tests = JSON.parse(data);
+    for (let i = 0; i < tests.length; ++i) {
+        let name = tests[i]["name"];
+        let ok = tests[i]["ok"];
+        let grammar = tests[i]["grammar"];
+        let accepted = false;
+        try {
+            let G = new Grammar_1.Grammar(grammar);
+            accepted = true;
+        }
+        catch (e) {
+        }
+        if (ok) {
+            if (!accepted) {
+                console.log("Rejected valid grammar " + name);
+                return;
+            }
+        }
+        else {
+            if (accepted) {
+                console.log("Accepted invalid grammar " + name);
+                return;
+            }
+        }
+    }
+    console.log("All tests OK");
+    return true;
+}
 console.log('Hello world');
+main();
 //# sourceMappingURL=app.js.map
