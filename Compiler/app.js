b37a4e6b860a2caedddff3553ad3f934e9593ef6
Alex <kjakarproductions@gmail.com>
2020-04-19 01:44:22 -0400

    Finished lab 01

--- Compiler/app.js
+++ Compiler/app.js
@@ -5,80 +5,31 @@ const Grammar_1 = require("./Grammar");
 function main() {
     let data = fs.readFileSync("tests.txt", "utf8");
     let tests = JSON.parse(data);
-    let numPassed = 0;
-    let numFailed = 0;
     for (let i = 0; i < tests.length; ++i) {
         let name = tests[i]["name"];
-        if (true) //name == "g6.txt") 
-         {
-            let expected = tests[i]["follow"];
-            let input = tests[i]["input"];
-            let G = new Grammar_1.Grammar(input);
-            let first = G.getFollow();
-            if (!dictionariesAreSame(expected, first)) {
-                console.log("Test " + name + " failed");
-                ++numFailed;
-                console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TEST FAILED : " + name + "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        let ok = tests[i]["ok"];
+        let grammar = tests[i]["grammar"];
+        let accepted = false;
+        try {
+            let G = new Grammar_1.Grammar(grammar);
+            accepted = true;
+        }
+        catch (e) {
+        }
+        if (ok) {
+            if (!accepted) {
+                console.log("Rejected valid grammar " + name);
+                return;
             }
-            else {
-                console.log("################################ TEST PASSESD : " + name + "################################");
-                ++numPassed;
+        }
+        else {
+            if (accepted) {
+                console.log("Accepted invalid grammar " + name);
+                return;
             }
         }
     }
-    console.log(numPassed + " tests OK" + "      " + numFailed + " tests failed");
-    return numFailed == 0;
-}
-function dictionariesAreSame(s1, s2) {
-    let M1 = toMap(s1);
-    let M2 = s2;
-    let k1 = [];
-    let k2 = [];
-    for (let k of M1.keys())
-        k1.push(k);
-    for (let k of M2.keys())
-        k2.push(k);
-    k1.sort();
-    k2.sort();
-    if (!listsEqual(k1, k2)) {
-        console.log("Lists not equal:", k1, k2);
-        return false;
-    }
-    for (let k of k1) {
-        if (!listsEqual(M1.get(k), M2.get(k))) {
-            console.log("Lists not equal:", M1.get(k), M2.get(k));
-            return false;
-        }
-    }
-    return true;
-}
-function toMap(s) {
-    let r = new Map();
-    for (let k in s) {
-        r.set(k, new Set());
-        s[k].forEach((x) => {
-            r.get(k).add(x);
-        });
-    }
-    return r;
-}
-function listsEqual(L1a, L2a) {
-    let L1 = [];
-    let L2 = [];
-    L1a.forEach((x) => {
-        L1.push(x);
-    });
-    L2a.forEach((x) => {
-        L2.push(x);
-    });
-    L1.sort();
-    L2.sort();
-    if (L1.length !== L2.length)
-        return false;
-    for (let i = 0; i < L1.length; ++i) {
-        if (L1[i] !== L2[i])
-            return false;
-    }
+    console.log("All tests OK");
     return true;
 }
 main();
